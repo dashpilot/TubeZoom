646aa34e1613a9e341f632078e09c295e08d496a
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2022-07-21 01:20:34 +0200

    Create contentScript.js

---
+++ contentScript.js
@@ -0,0 +1,37 @@
+(() => { 
+
+const newVideoLoaded = async () => {
+  console.log('New video loaded');
+  
+  /*
+    const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+
+    currentVideoBookmarks = await fetchBookmarks();
+
+    if (!bookmarkBtnExists) {
+      const bookmarkBtn = document.createElement("img");
+
+      bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+      bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+      bookmarkBtn.title = "Click to bookmark current timestamp";
+
+      youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+      youtubePlayer = document.getElementsByClassName('video-stream')[0];
+
+      youtubeLeftControls.appendChild(bookmarkBtn);
+      bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+    }
+    */
+  };
+
+  chrome.runtime.onMessage.addListener((obj, sender, response) => {
+    const { type, value, videoId } = obj;
+
+    if (type === "NEW") {
+      currentVideo = videoId;
+      newVideoLoaded();
+    }
+  });
+
+  newVideoLoaded();
+})();
